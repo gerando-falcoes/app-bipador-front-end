b7fa9a56792782502f7651dcf797f2492cccc163
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-05-04 10:17:05 -0300

    Changing button position

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,27 +1,25 @@
 import React, { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth'
-
+import { AuthContext } from '../../contexts/auth';
 
 const HomePage = () => {
-
-    const local =JSON.parse(localStorage.getItem('user'))
+    const local = JSON.parse(localStorage.getItem('user'))
     console.log(local.category)
     
-    const { logout, redirectBip} = useContext(AuthContext)
+    const { logout, redirectBip } = useContext(AuthContext)
 
     const handleLogout = () => {
         logout()
     }
 
     const handleVoltarParaBipagem = () => {
-        redirectBip(local.email, local.password,local.category)
+        redirectBip(local.email, local.password, local.category)
     }
 
     return (
         <>
-            <h1>home page</h1>
+            <h1>Home Page</h1>
+            <button onClick={handleVoltarParaBipagem}>Ir para o Bip</button>
             <button onClick={handleLogout}>Logout</button>
-            <button onClick={handleVoltarParaBipagem}>ir para o bip</button>
         </>
     )
 }
