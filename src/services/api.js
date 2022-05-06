93ff6eaf133375b2d4e7548e1b1b162a373a9697
Erickinhou <willianerick17@gmail.com>
2022-05-06 13:58:23 -0300

    fix: change baseURL name

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
-const nuvem = 'https://coletor-api.azurewebsites.net/'
-// const local = 'http://localhost:3000'
+import axios from "axios";
+// const baseURL = 'https://coletor-api.azurewebsites.net/'
+const baseURL = "http://localhost:3000";
 
 const api = axios.create({
-  baseURL: nuvem
+  baseURL,
 });
 
 export default api;
