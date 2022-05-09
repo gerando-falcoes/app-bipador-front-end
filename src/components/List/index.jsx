41b0638e00724bff902f896a69716f052860f60f
Erickinhou <willianerick17@gmail.com>
2022-05-09 14:39:27 -0300

    fix: improve list to use Table component

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,62 +1,22 @@
 import PropTypes from "prop-types";
-// import Button from '../Button'
-// import coracaoImg from '../../assets/coracao.svg'
-// import { Link } from "react-router-dom";
-import Table from "react-bootstrap/Table";
-// import '../List/List.css'
+import Table from "../Table";
+import "../List/List.css";
 
 const Post = (props) => {
-  // const data = new Date(props.data).toLocaleDateString();
-
   return (
-    <article>
-      <div className="caixa">{/* <span>{data}</span> */}</div>
-      {
-        // data ?
-        //   <div>
-        //     <h3 className="nome">{props.nome}</h3>
-        //     <p>codigo de barra {props.id}</p>
-        //     <p>quantidade: {props.quantidade}</p>
-        //     <p>preço: {props.preco}</p>
-        //   </div> : data
-      }
-      <Table striped bordered hover responsive="sm">
-        <thead>
-          <tr>
-            <th>Code</th>
-            <th>Produto</th>
-            <th>Preço</th>
-            <th>Quantidade</th>
-            <th>Deletar</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{props.id}</td>
-            <td>{props.nome}</td>
-            <td>{props.preco}</td>
-            <td>{props.quantidade}</td>
-            <td>
-              <button
-                type="button"
-                onClick={props.delet}
-                value={props.index}
-                className="btn btn-danger"
-              >
-                Deletar
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-
-      {/* Definir Classe CSS */}
+    <article className="list">
+      <Table data={props} />
     </article>
   );
 };
 
 Post.propTypes = {
+  id: PropTypes.string,
   nome: PropTypes.string,
+  preco: PropTypes.string,
+  quantidade: PropTypes.number,
+  delet: PropTypes.func,
+  index: PropTypes.number,
 };
 
 export default Post;
