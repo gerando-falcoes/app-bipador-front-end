bd26122f4cc771c23717870c5df638a4f0b7e2eb
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-05-16 18:07:14 -0300

    fix conflicts

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -2,51 +2,65 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "./table.module.css";
 
-const Table = ({ data }) => {
+const Table = ({posts, onDelete}) => {
+  let totalProducts = 0;
   return (
-    <div className={style.table}>
-      <div className={style.section}>
-        <div className={style.title}>Code</div>
-        <div className={style.value}>{data.id}</div>
-      </div>
-      <div className={style.section}>
-        <div className={style.title}>Produto</div>
-        <div className={style.value}>{data.nome}</div>
-      </div>
-      <div className={style.section}>
-        <div className={style.title}>Preço</div>
-        <div className={style.value}>{data.preco}</div>
-      </div>
-      <div className={style.section}>
-        <div className={style.title}>Quantidade</div>
-        <div className={style.value}>{data.quantidade}</div>
-      </div>
-      <div className={style.section}>
-        <div className={style.title}>Deletar</div>
-        <div className={style.delete_button}>
-          <button
-            type="button"
-            onClick={data.delete}
-            value={data.index}
-            className="btn btn-danger"
-          >
-            <img src="/images/trash.svg" alt="delete icon" />
-          </button>
-        </div>
-      </div>
-    </div>
+    <> 
+      {posts.map((product, index) => {
+        totalProducts += Number(product.quantidade)
+        return (
+          <div className={style.table}>           
+            <div className={style.section}>
+              <div className={style.title}>Code</div>
+              <div className={style.value}>{product.id_produto}</div>
+            </div>
+            <div className={style.section}>
+              <div className={style.title}>Produto</div>
+              <div className={style.value}>{product.nome}</div>
+            </div>
+            <div className={style.section}>
+              <div className={style.title}>Preço</div>
+              <div className={style.value}>{product.preco}</div>
+            </div>
+            <div className={style.section}>
+              <div className={style.title}>Quantidade</div>
+              <div className={style.value}>{product.quantidade}</div>
+            </div>
+            <div className={style.section}>
+              <div className={style.title}>Deletar</div>
+              <div className={style.delete_button}>
+                <button
+                  type="button"
+                  onClick={() => onDelete(index)}
+                  className="btn btn-danger"
+                >
+                  <img src="/images/trash.svg" alt="delete icon" />
+                </button>
+              </div>
+            </div>
+          </div>  
+        )
+        })}
+        <div className={style.table}>
+          <div className={style.section}>
+            <div className={`${style.total_products} text-center`}>Total de Produtos: {totalProducts}</div>
+          </div>
+        </div>    
+    </>
   );
 };
+   
 
 Table.propTypes = {
-  data: PropTypes.shape({
-    id: PropTypes.string,
-    nome: PropTypes.string,
-    preco: PropTypes.string,
-    quantidade: PropTypes.number,
-    delet: PropTypes.func,
-    index: PropTypes.number,
-  }),
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    id_produto: PropTypes.string.isRequired,
+    nome: PropTypes.string.isRequired,
+    preco: PropTypes.string.isRequired,
+    quantidade: PropTypes.number.isRequired,
+  })),
+
+  onDelete: PropTypes.func.isRequired,
+
 };
 
 export default Table;
