3b641200682a26c4d43048deb731f9012fe9560d
Erickinhou <willianerick17@gmail.com>
2022-05-23 10:24:54 -0300

    fix: add new login style

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-const baseURL = "https://coletor-api.azurewebsites.net/";
-// const baseURL = "http://localhost:3000";
+import axios from 'axios'
+const baseURL = 'https://coletor-api.azurewebsites.net/'
+// const baseURL = 'http://localhost:3000'
 
 const api = axios.create({
   baseURL,
-});
+})
 
-export default api;
+export default api
