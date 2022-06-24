7b9ecf8e8c33d540261cbdcdc28922241cac42d8
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-06-24 16:48:32 -0300

    feat: logout confirmation modal

---
+++ src/components/LogoutModal/index.js
@@ -0,0 +1,23 @@
+import { Modal, Button } from "react-bootstrap";
+ 
+const LogoutModal = ({ onShowModal, onHideModal, onConfirmModal, message }) => {
+
+    return (
+        <Modal show={onShowModal} onHide={onHideModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Logout</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><div className="alert alert-danger">{message}</div></Modal.Body>
+        <Modal.Footer>
+          <Button variant="default" onClick={onHideModal}>
+            Cancelar
+          </Button>
+          <Button variant="danger" onClick={() => onConfirmModal()}>
+            Fazer Logout
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+}
+ 
+export default LogoutModal;
