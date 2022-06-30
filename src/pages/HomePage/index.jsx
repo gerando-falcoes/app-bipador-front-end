0c0470697d5ebe9009e1d5e403f8d771557129b7
MsCardoso77 <mathsoarescardoso@gmail.com>
2022-06-30 19:51:51 -0300

    feat: add logout confirmation modal

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,34 +1,64 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth';
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/auth'
 import { Button } from 'react-bootstrap'
 
 import './HomePage.css'
+import { useState } from 'react'
+import LogoutModal from '../../components/LogoutModal'
 
 const HomePage = () => {
-    const local = JSON.parse(localStorage.getItem('user'))
-    console.log(local.category)
-    
-    const { logout, redirectBip } = useContext(AuthContext)
+  const local = JSON.parse(localStorage.getItem('user'))
+  console.log(local.category)
 
-    const handleLogout = () => {
-        logout()
+  const { logout, redirectBip } = useContext(AuthContext)
+
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false)
+
+  const handleLogout = () => {
+    setIsLogoutModalOpen(false)
+    logout()
+  }
+
+  const onHideLogoutModal = () => {
+    setIsLogoutModalOpen(false)
+  }
+
+  const startLogoutModal = () => {
+    let products = JSON.parse(localStorage.getItem('Produto'))
+    if (products.length !== 0) {
+      setIsLogoutModalOpen(true)
+    } else if (products.length === 0) {
+      handleLogout()
     }
+  }
 
-    const handleVoltarParaBipagem = () => {
-        redirectBip(local.email, local.password, local.category)
-    }
+  const handleVoltarParaBipagem = () => {
+    redirectBip(local.email, local.password, local.category)
+  }
 
-    return (
-        <div className='container'>
-            <div className='home'>
-                <h1 className='title'>Home Page</h1>
-                <div className='buttonContainer'>
-                    <Button onClick={handleVoltarParaBipagem} variant="secondary">Ir para o Bip</Button>
-                    <Button onClick={handleLogout} variant="secondary">Logout</Button>
-                </div>
-            </div>
+  return (
+    <div className="homeContainer">
+      <div className="home">
+        <h1 className="title">Home Page</h1>
+        <div className="buttonContainer">
+          <Button onClick={handleVoltarParaBipagem} variant="secondary">
+            Ir para o Bip
+          </Button>
+          <Button onClick={startLogoutModal} variant="secondary">
+            Logout
+          </Button>
         </div>
-    )
+      </div>
+      <LogoutModal
+        onShowModal={isLogoutModalOpen}
+        onConfirmModal={handleLogout}
+        onHideModal={onHideLogoutModal}
+        message={
+          'Você possui produtos bipados que ainda não foram salvos. Realizar o logout irá resultar na perda dos mesmos.'
+        }
+      />
+    </div>
+  )
 }
 
 export default HomePage
